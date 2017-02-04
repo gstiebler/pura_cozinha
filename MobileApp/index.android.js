509121e743e4b0c92394dfcbfcd416e697f3c8bd
Guilherme Stiebler <guilhermemst@gmail.com>
2017-02-04 11:52:11 -0200

    Add tabs

--- MobileApp/index.android.js
+++ MobileApp/index.android.js
@@ -12,13 +12,29 @@ import {
   View
 } from 'react-native';
 import FoodMenu from './src/containers/FoodMenu';
+import Tabs from 'react-native-tabs';
 
 export default class MobileApp extends Component {
   render() {
     return (
-      <FoodMenu />
+      <View style={styles.container}>
+        <FoodMenu />
+        <Tabs selected="first" style={{backgroundColor:'white'}}
+              selectedStyle={{color:'red'}} onSelect={el=>console.log(el.props.name)}>
+          <Text name="first">First</Text>
+          <Text name="second">Second</Text>
+          <Text name="third">Third</Text>
+        </Tabs>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+});
 
 AppRegistry.registerComponent('MobileApp', () => MobileApp);
