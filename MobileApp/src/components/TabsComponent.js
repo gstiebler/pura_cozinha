f531e04af529b11bcf33f20760fcb21f8c30c585
Guilherme Stiebler <guilhermemst@gmail.com>
2017-02-06 19:59:15 -0200

    Showing more than one tab

--- MobileApp/src/components/TabsComponent.js
+++ MobileApp/src/components/TabsComponent.js
@@ -9,12 +9,25 @@ import FoodMenu from '../../src/containers/FoodMenu';
 import Tabs from 'react-native-tabs';
 
 export default class TabsComponent extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { page:'food_menu' };
+  }
+
   render() {
+    let viewedTab = null;
+    if(this.state.page == 'food_menu') {
+      viewedTab = <FoodMenu />
+    } else if (this.state.page == 'orders') {
+      viewedTab = <Text>Pedidos são mostrados aqui</Text>
+    }
+
     return (
       <View style={styles.container}>
-        <FoodMenu />
-        <Tabs selected="first" style={{backgroundColor:'white'}}
-              selectedStyle={{color:'red'}} onSelect={el=>console.log(el.props.name)}>
+        { viewedTab }
+        <Tabs selected={this.state.page} style={styles.tabs}
+              selectedStyle={styles.selected} onSelect={el=>this.setState({page:el.props.name})}>
           <Text name="food_menu">Cardápio</Text>
           <Text name="orders">Pedidos</Text>
           <Text name="profile">Eu</Text>
@@ -29,4 +42,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
+  selected: {
+    color:'red'
+  },
+  tabs: {
+    backgroundColor:'white'
+  }
 });
