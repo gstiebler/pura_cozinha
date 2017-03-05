3a0d4301a803a88c8f4496e3f8ffd54092f1f359
Guilherme Stiebler <guilhermemst@gmail.com>
2017-03-05 16:57:45 -0300

    Adding Winston error logging for Express

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
+import * as winston from 'winston';
 
 import { schema } from './graphql_controller';
 import * as db from './db';
@@ -12,5 +13,43 @@ app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err:any = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req: express.Request, res: express.Response) {
+    winston.error(err.stack);
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req: express.Request, res: express.Response) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+process.on('uncaughtException', function (err) {
+    winston.error(err.stack);
+})
 
 app.listen(4000);
