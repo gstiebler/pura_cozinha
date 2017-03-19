a4c9a4ea7980c9976a2db75c0ee257a82a969c72
Guilherme Stiebler <guilhermemst@gmail.com>
2017-03-19 17:06:41 -0300

    Showing menu items on a table

--- WebAdmin/src/components/FoodMenuItems.tsx
+++ WebAdmin/src/components/FoodMenuItems.tsx
@@ -1,10 +1,44 @@
 import * as React from 'react';
+import { Table, Column, Cell } from 'fixed-data-table';
+import { formatCurrency } from '../lib/StringUtils';
 
 export default function FoodMenuItems({ items }) {
-
-  const htmlItems = items.map(item => { return <div key={item._id}>{item.title}</div> });
+  // const htmlItems = items.map(item => { return <div key={item._id}>{item.title}</div> });
 
   return (
-    <div>{ htmlItems }</div>
+    <Table
+      rowsCount={items.length}
+      rowHeight={50}
+      headerHeight={50}
+      width={800}
+      height={500}>
+      <Column
+        header={<Cell>Item</Cell>}
+        cell={props => (
+          <Cell {...props}>
+            {items[props.rowIndex].title}
+          </Cell>
+        )}
+        width={200}
+      />
+      <Column
+        header={<Cell>Descrição</Cell>}
+        cell={props => (
+          <Cell {...props}>
+            {items[props.rowIndex].description}
+          </Cell>
+        )}
+        width={400}
+      />
+      <Column
+        header={<Cell>Preço</Cell>}
+        cell={props => (
+          <Cell {...props}>
+            {formatCurrency(items[props.rowIndex].price)}
+          </Cell>
+        )}
+        width={200} align='right'
+      />
+    </Table>
   );
 };
