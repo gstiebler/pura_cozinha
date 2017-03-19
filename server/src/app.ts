6d37175f981a8922fcdf5cb422b9e10a723f5ede
Guilherme Stiebler <guilhermemst@gmail.com>
2017-03-18 22:07:44 -0300

    Serving web admin app static files

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
 import * as winston from 'winston';
+import * as path from 'path';
 
 import { schema } from './graphql_controller';
 import * as db from './db';
@@ -23,6 +24,11 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
+app.use('/admin_app', express.static(path.join(__dirname, '/../../WebAdmin/dist')));
+app.use('/admin_app/ext_libs', express.static(path.join(__dirname, '/../../WebAdmin/node_modules')));
+app.get('/admin_app/*', function (req, res) {
+  return res.redirect('/admin_app/?url=' + encodeURIComponent(req.url));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
