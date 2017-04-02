214ae6330175c7ccc99321e096e28f1c12e348c7
Guilherme Stiebler <guilhermemst@gmail.com>
2017-04-02 18:09:07 -0300

    Fix tsconfig

--- MobileApp/index.android.js
+++ MobileApp/index.android.js
@@ -6,7 +6,7 @@
 
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import MainComponent from './out/src/components/MainComponent';
+import MainComponent from './out/components/MainComponent';
 
 export default class MobileApp extends Component {
   render() {
