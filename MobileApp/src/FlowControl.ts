f96783817588cb7fe6c4da0f5f89025d99d4fcba
Guilherme Stiebler <guilhermemst@gmail.com>
2017-04-14 15:02:06 -0300

    Removing the tab from the mobile app

--- MobileApp/src/FlowControl.ts
+++ MobileApp/src/FlowControl.ts
@@ -1,21 +1,17 @@
 import { BackAndroid } from 'react-native';
 
 let _navigator = null;
-let _tabComponent = null;
 
 export function setNavigator(navigator) {
   _navigator = navigator;
 }
 
-export function setTabComponent(tabComponent) {
-  _tabComponent = tabComponent;
-}
 
 /** Flow controls */
 
 export const kitchensFlowControl = {
   onKitchenSelected: (id: string) => {
-    _tabComponent.setPage('food_menu');
+    _navigator.push({id: 'food_menu' });
   }
 };
 
--- MobileApp/src/components/MainComponent.tsx
+++ MobileApp/src/components/MainComponent.tsx
@@ -6,23 +6,26 @@ import {
   Navigator
 } from 'react-native';
 import Header from './Header';
-import TabsComponent from './TabsComponent';
 import FoodMenuItem from '../containers/FoodMenuItem';
 import Address from '../containers/Address';
 import CreditCard from '../containers/CreditCard';
 import Kitchens from '../containers/KitchensContainer';
+import FoodMenu from '../containers/FoodMenu';
+import Cart from '../containers/Cart';
 import * as FlowControl from '../FlowControl';
 
 function router(route, navigator) {
   FlowControl.setNavigator(navigator);
-  if (route.id === 'tabs') {
-    return <TabsComponent />;
+   if (route.id === 'kitchens') {
+      return <Kitchens />;
   } else if (route.id === 'menu_item') {
-    return <FoodMenuItem
-      menuItemId={route.menuItemId}
-    />
+    return <FoodMenuItem menuItemId={route.menuItemId} />;
+  } else if (route.id === 'food_menu') {
+      return <FoodMenu />;
   } else if (route.id  === 'address') {
     return <Address />;
+  } else if (route.id  === 'cart') {
+    return <Cart />;
   } else if (route.id  === 'credit_card') {
     return <CreditCard />;
   } else {
@@ -35,7 +38,7 @@ const MainComponent = () => {
 
   return (
     <Navigator
-        initialRoute={{ id: 'tabs' }}
+        initialRoute={{ id: 'kitchens' }}
         renderScene={(route, navigator) =>
           <View style={styles.container}>
             <Header/>
