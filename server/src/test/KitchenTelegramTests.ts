031cedfcc92bdbc0411b948bdc676e6d5b83e173
Guilherme Stiebler <guilhermemst@gmail.com>
2017-05-24 13:22:04 -0300

    Testing setting an order as ready

--- server/src/test/KitchenTelegramTests.ts
+++ server/src/test/KitchenTelegramTests.ts
@@ -2,6 +2,7 @@ import * as assert from 'assert';
 import execFixtures from './fixtures/fixture';
 import { KitchenBotLogic } from '../KitchenTelegram/KitchenBotLogic';
 import { Kitchen } from '../db/models/kitchen';
+import { Order, PaymentStatus } from '../db/models/Order';
 import { MenuItem } from '../db/models/menuItem';
 
 
@@ -46,6 +47,25 @@ describe('Kitchen Telegram', function() {
     const kitchen1: any = await Kitchen.findOne({ name: 'Cozinha do Marcel' });
     assert.equal('NO', kitchen1.active);
 
+    // Ask to change an order as ready
+    await logic.receive({ text: 'Definir pedido como pronto' });
+    assert.equal('Pedido a ser marcado como pronto:\n1) Sanduba de frango: 1\n', lastResponse().msg);
+    replay_markup = JSON.parse(lastResponse().options.reply_markup);
+    assert.equal(1, replay_markup.inline_keyboard.length);
+    assert.equal('1', replay_markup.inline_keyboard[0][0].text);
+    assert.equal('0', replay_markup.inline_keyboard[0][0].callback_data);
+
+    // Select the order to set as ready
+    await logic.callbackQuery({ data: '0' });
+    assert.equal('Cozinha está inativa', lastResponse().msg);
+    replay_markup = JSON.parse(lastResponse().options.reply_markup);
+    assert.equal(3, replay_markup.keyboard.length);
+    assert.equal('Definir cozinha como ativa', replay_markup.keyboard[0][0].text);
+    assert.equal('Modificar estoque', replay_markup.keyboard[1][0].text);
+    assert.equal('Definir pedido como entregue', replay_markup.keyboard[2][0].text);
+    const paidOrders = await Order.find({ status: PaymentStatus.PAYMENT_OK, kitchen: kitchen1._id });
+    assert.equal(0, paidOrders.length);
+
     // Show items in stock
     await logic.receive({ text: 'Modificar estoque' });
     assert.equal('Estoque:\nSanduba de frango: 8\nAçai: 0\nSanduíche de Mignon: 7\n', lastResponse().msg);
@@ -65,9 +85,10 @@ describe('Kitchen Telegram', function() {
     assert.equal('Quantidade de Sanduba de frango atualizada para 6', responses[responses.length - 2].msg);
     assert.equal('Cozinha está inativa', lastResponse().msg);
     replay_markup = JSON.parse(lastResponse().options.reply_markup);
-    assert.equal(2, replay_markup.keyboard.length);
+    assert.equal(3, replay_markup.keyboard.length);
     assert.equal('Definir cozinha como ativa', replay_markup.keyboard[0][0].text);
     assert.equal('Modificar estoque', replay_markup.keyboard[1][0].text);
+    assert.equal('Definir pedido como entregue', replay_markup.keyboard[2][0].text);
   });
 
 });
