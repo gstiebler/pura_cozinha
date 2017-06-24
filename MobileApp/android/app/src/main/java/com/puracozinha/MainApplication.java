08b5a8922e4b5b19ab71c4377798d05c292dcedb
Guilherme Stiebler <guilhermemst@gmail.com>
2017-06-24 18:59:25 -0300

    Adding paypal and... updating react-native and the name of the app

--- MobileApp/android/app/src/main/java/com/puracozinha/MainApplication.java
+++ MobileApp/android/app/src/main/java/com/puracozinha/MainApplication.java
@@ -1,11 +1,10 @@
 package com.puracozinha;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.taessina.paypal.RNPaypalWrapperPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -18,7 +17,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
@@ -26,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPaypalWrapperPackage(),
             new ReactNativePushNotificationPackage()
       );
     }
