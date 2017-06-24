40b0eeb23630428c6033db65b738c6873689046f
Guilherme Stiebler <guilhermemst@gmail.com>
2017-06-24 19:52:02 -0300

    Adding the app.env file

--- MobileApp/src/Startup.ts
+++ MobileApp/src/Startup.ts
@@ -2,9 +2,10 @@ import { AsyncStorage } from 'react-native';
 import { getGeolocation} from './lib/geolocation';
 import { Model } from './Model';
 import { Network } from './Network';
+import * as ENV from './app.env';
 
 const port = '3000';
-const baseURL = 'http://192.168.0.14:' + port + '/graphql';
+const baseURL = 'http://' + ENV.VARS.DEV_SERVER_IP + ':' + port + '/graphql';
 const network = new Network(baseURL, fetch);
 const model = new Model(network, AsyncStorage, getGeolocation);
 
