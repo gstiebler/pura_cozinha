dcf6f2bd7fd6abb05cb2b5ec627225b8c7d25e7a
Guilherme <guilhermemst@gmail.com>
2018-03-03 21:06:55 -0300

    Adding kitchen app build

--- infrastructure/startServer.sh
+++ infrastructure/startServer.sh
@@ -2,5 +2,8 @@
 cd ConsumerWebApp
 node ../node_modules/webpack/bin/webpack.js --config webpack.prod.js
 cd ..
+cd KitchenWebApp
+node ../node_modules/webpack/bin/webpack.js --config webpack.prod.js
+cd ..
 node ./node_modules/gulp/bin/gulp.js transpile
 node ./node_modules/gulp/bin/gulp.js start:server
