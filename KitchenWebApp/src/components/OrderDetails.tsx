9d5998e191fd9870f2e848ccedb783b7e339904c
Guilherme <guilhermemst@gmail.com>
2018-03-04 23:18:26 -0300

    Order details styling

--- KitchenWebApp/src/components/OrderDetails.tsx
+++ KitchenWebApp/src/components/OrderDetails.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { observer } from 'mobx-react';
 import { Store } from '../model/Store';
 import Typography from 'material-ui/List';
+import Paper from 'material-ui/Paper';
 import { withStyles } from 'material-ui/styles';
 import { formatCurrency } from '../../../common/util';
 import * as moment from 'moment';
@@ -9,6 +10,7 @@ import * as moment from 'moment';
 const styles = {
   root: {
     padding: 16,
+    margin: 8,
   }
 };
 
@@ -24,16 +26,18 @@ function OrderDetails(props: IProps) {
     return <div />;
   }
   return (
-    <div className={classes.root}>
-      <Typography component="p">
-        Status: {order.readableStatus}
-      </Typography>
-      <Typography component="p">
-        Local: {order.local}
-      </Typography>
-      <Typography component="p">
-        Complemento: {order.localComplement}
-      </Typography>
+    <div>
+      <Paper className={classes.root} elevation={4}>
+        <Typography component="p">
+          Status: {order.readableStatus}
+        </Typography>
+        <Typography component="p">
+          Local: {order.local}
+        </Typography>
+        <Typography component="p">
+          Complemento: {order.localComplement}
+        </Typography>
+      </Paper>
     </div>
   );
 }
