90b0403ad0d8ab11b7f889b0ad920c399e2b78fd
Guilherme <guilhermemst@gmail.com>
2018-03-10 17:25:19 -0300

    Displaying drawer icon on navbar

--- KitchenWebApp/src/components/Navbar.tsx
+++ KitchenWebApp/src/components/Navbar.tsx
@@ -5,6 +5,9 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import * as classNames from 'classnames';
 import { withStyles } from 'material-ui/styles';
 import { Link } from 'mobx-router';
 import Views from '../Views';
@@ -20,6 +23,9 @@ const styles = {
     marginLeft: -12,
     marginRight: 20,
   },
+  hide: {
+    display: 'none',
+  },
 };
 
 interface IProps {
@@ -29,9 +35,17 @@ interface IProps {
 
 function Navbar(props: IProps) {
   const { classes, store } = this.props;
+  const open = false;
   return (
     <AppBar className={classes.root} position="static" color="default">
       <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={this.handleDrawerOpen}
+          className={classNames(classes.menuButton, open && classes.hide)} >
+          <MenuIcon />
+        </IconButton>
         <Typography variant="title" color="inherit">
           Admin Cozinha
         </Typography>
