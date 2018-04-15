c567c2c2dfb5a57d518fc06b1ae2fd0d0dce0800
Guilherme <guilhermemst@gmail.com>
2018-04-15 20:10:20 -0300

    Making consumer app graphql queries more explicit

--- ConsumerWebApp/src/model/NetworkServices.ts
+++ ConsumerWebApp/src/model/NetworkServices.ts
@@ -8,16 +8,62 @@ import {
 import { objToGrahqlStr } from '../../../common/util';
 
 export async function fetchFoodMenu(): Promise<FoodMenuItem[]> {
-  const fields = `_id, title, price, description, imgURL, 
-      boolOptions { label, key }, 
-      options { key, label, optionItems { key, label } }`;
-  const query = `query { menuItems(lat: ${0.0}, lng: ${0.0}) { ${fields} } }`;
+  const query = `
+    query {
+      menuItems( lat: ${0.0}, lng: ${0.0} ) { 
+        _id, 
+        title, 
+        price, 
+        description, 
+        imgURL, 
+        boolOptions {  label,  key  }, 
+        options { 
+          key, 
+          label, 
+          optionItems { key,  label }
+        }
+      } 
+    }
+  `;
   const result = await network.fetchQuery(query);
   return result.menuItems;
 }
 
 export async function sendOrderRequest(orderRequest: IOrderRequest) {
-  const mutation = `mutation { saveOrder( fmiData: ${objToGrahqlStr(orderRequest)} ) }`;
+  const items = orderRequest.orderSummary.items.map(item => {
+    return `
+      {
+        fmi: {
+          _id: "${item.fmi._id}",
+          title: "${item.fmi.title}",
+          description: "${item.fmi.description}",
+          price: ${item.fmi.price},
+          imgURL: "${item.fmi.imgURL}",
+          selectedOptions: [${item.fmi.selectedOptions}],
+          selectedBoolOptions: [${item.fmi.selectedBoolOptions}]
+        },
+        qty: ${item.qty},
+        itemTotalPrice: ${item.itemTotalPrice}
+      }
+    `;
+  });
+  const mutation = `
+    mutation {
+      saveOrder (
+        fmiData: {
+          orderSummary: {
+            items: [
+              ${items.join(',')}
+            ],
+            totalAmount: ${orderRequest.orderSummary.totalAmount}
+          },
+          local: "${orderRequest.local}",
+          paymentOption: "${orderRequest.paymentOption}",
+          telephoneNumber: "${orderRequest.telephoneNumber}"
+        }
+      ) 
+    }
+  `;
   const result = await network.fetchQuery(mutation);
   return result.msg;
 }
