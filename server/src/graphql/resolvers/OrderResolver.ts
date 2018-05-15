8efa28bd0dfbc88db23e16aaae9fa4df25ea3d1d
Guilherme Stiebler <guilhermemst@gmail.com>
2018-05-14 22:31:27 -0300

    Adding mock validate order function

--- server/src/graphql/resolvers/OrderResolver.ts
+++ server/src/graphql/resolvers/OrderResolver.ts
@@ -2,8 +2,25 @@ import { IOrderRequest } from '../../../../common/Interfaces';
 import { Order, IOrder, IOrderItem } from '../../db/models/Order';
 import * as Twitter from '../../lib/Twitter';
 import * as OrderProcess from '../../core/OrderProcess';
+import * as logger from 'winston';
+import * as Bluebird from 'bluebird';
+import { MenuItem } from '../../db/models/menuItem';
+
+/**
+ * Return true if order is valid
+ * @param fmiData 
+ */
+async function validateOrder(fmiData: IOrderRequest):Promise<boolean> {
+  // TODO: implement validation
+  return true;
+}
 
 export async function saveOrder(fmiData: IOrderRequest) {
+  const isOrderValid = await validateOrder(fmiData);
+  if (!isOrderValid) {
+    logger.error('Order not valid!');
+    throw new Error('Order not valid!');
+  }
   const items:IOrderItem[] = fmiData.orderSummary.items.map(item => ({
     qty: item.qty,
     itemTotalPrice: item.itemTotalPrice,
