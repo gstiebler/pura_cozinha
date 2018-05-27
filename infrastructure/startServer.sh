e9cf866cfb5ad318f37988ebe18ae05f691fdc98
Guilherme Stiebler <guilhermemst@gmail.com>
2018-05-27 18:10:36 -0300

    Adding compilation of AdminApp to scripts

--- infrastructure/startServer.sh
+++ infrastructure/startServer.sh
@@ -5,5 +5,8 @@ cd ..
 cd KitchenWebApp
 node ../node_modules/webpack/bin/webpack.js --config webpack.prod.js
 cd ..
+cd WebAdminApp
+node ../node_modules/webpack/bin/webpack.js --config webpack.prod.js
+cd ..
 node ./node_modules/gulp/bin/gulp.js transpile
 node ./node_modules/gulp/bin/gulp.js start:server
