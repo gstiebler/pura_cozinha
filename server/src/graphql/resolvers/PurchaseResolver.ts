3150cec939a5b24a08336b0b9408ca5f475dda1e
Unknown <nigelnaiguel.comp@gmail.com>
2018-06-15 23:10:55 -0300

    Bug fix in the calculation of ingredient type stock and purchase

--- server/src/graphql/resolvers/PurchaseResolver.ts
+++ server/src/graphql/resolvers/PurchaseResolver.ts
@@ -6,34 +6,36 @@ import { MenuItem } from "../../db/models/menuItem";
 import { TOrderStatus, IFoodMenuItem } from "../../../../common/Interfaces";
 import * as Bluebird from 'bluebird';
 import * as kitchenNs from "../../../../KitchenWebApp/src/model/NetworkServices";
+import { Map } from 'immutable'; 
 
 
 export async function getIngredientTypesStocks() {
   //Get prepared items
   const closedStatuses: TOrderStatus[] = ['DELIVERING', 'DELIVERED', 'PENDING', 'PREPARING', ];
   const query = { status: { $in: closedStatuses } };
-  const menuItems: any[] = [];
   
-  const orders = await Order.find(query);
-
-  await orders.forEach(async order => {
-    const promises = order.items.map(async item => {
-      const menuItem = await MenuItem.findOne({_id: item.foodMenuItem.id});  
-      menuItems.push(menuItem.toObject());
-    });
-    await Promise.all(promises);
-  });
-
-  const ingredientTypes = await IngredientType.find();
-
-  const stocks = await ingredientTypes.map(async ingredient => {
-    const editedStock = await KitchenStock.findOne({ ingredientType: ingredient._id });
+  const {  
+    orders,  
+    menuItems,  
+    ingredientTypes, 
+    kitchenStocks
+  } = await Bluebird.props({ 
+    orders: Order.find(query), 
+    menuItems: MenuItem.find(), 
+    ingredientTypes: IngredientType.find(), 
+    kitchenStocks: KitchenStock.find()
+  }); 
+  
+  const purchasesStock = await Bluebird.map(ingredientTypes, async ingredient => {
+    const editedStock = kitchenStocks.find(ks => ks.toObject().ingredientType == ingredient._id.toString());
     let updated = new Date('1970-01-01');
     let finalTotal = 0;
+    // console.log('edited stock ' + (kitchenStocks[0].toObject().ingredientType == ingredient._id.toString()));
     if(editedStock)
     {
       updated = editedStock.toObject().updatedAt;
       finalTotal = editedStock.toObject().quantity;
+      console.log('final total ' + finalTotal);
     }
 
     const ingredientStock = await Purchase.aggregate([
@@ -43,32 +45,59 @@ export async function getIngredientTypesStocks() {
 
     if(ingredientStock)
       finalTotal += ingredientStock[0].total;
-    console.log('_id ' + ingredient._id + ', ' + finalTotal);
+    
     return {
       _id: ingredient._id,
       total: finalTotal
     }
   });
+  purchasesStock.map(p => console.log('purchases '+p.total + ' ' + p._id));
+  const menuItemsMap = Map<string, IFoodMenuItem>(menuItems.map(m => [m._id.toString(), m])); 
+ 
+  const zeroStockArray = ingredientTypes.map(i => [i._id, 0.0]); 
+  const zeroStockIngredients = Map<string, number>(zeroStockArray);
 
-  console.log('Final ' + stocks);
-  
-  const stockTemp = await Purchase.aggregate([
-    { $group: { _id: "$ingredientType", total: { $sum: "$quantity" } } },
-    { $sort: { total: -1 } }
-  ]);
-  
-  const ingredientTypesStock = stockTemp.reduce( (previous, stock) => {
-    let totalUsed = 0;
-    menuItems.forEach(menuItem => {
-      menuItem.usedIngredients.forEach( usedIngredient=> {
-        if(usedIngredient.ingredient == stock._id)
-          totalUsed += usedIngredient.quantity;
-      });
-    });
-    stock.total -= totalUsed;
-    previous.push(stock);
-    return previous;
-  }, []);
+  // calculate the stock decreased by the orders 
+  // reduce from the orders array 
+  const stockFromOrders = orders.reduce((aggregate, order) => { 
+    
+    // reduce from the items of the current order 
+    const reducedItems = order.items.reduce((menuAggregate, orderMenu) => { 
+      
+      const menuItem: IFoodMenuItem = menuItemsMap.get(orderMenu.foodMenuItem.id.toString()); 
+      console.log('Used ingredients : ' + menuItem);
+      // reduce from the ingredients of the menu item 
+      const reducedIngredient = menuItem.usedIngredients.reduce((menuItemAgg: Map<any, number>, ingredient) => { 
+        const editedStock = kitchenStocks.find(ks => ks.toObject().ingredientType == ingredient.ingredient);
+        
+        const previousIngQty = menuItemAgg.get(ingredient.ingredient); 
+        let totalLeft = 0;
+        console.log('Ingrediente em loop: ' + ingredient.quantity);
 
-  return ingredientTypesStock;
+        if(editedStock)
+          totalLeft = (order.createdOn >= editedStock.toObject().buyDate) ? previousIngQty - ingredient.quantity : previousIngQty;
+        else 
+          totalLeft = previousIngQty - ingredient.quantity;
+
+        return menuItemAgg.set(ingredient, totalLeft); 
+      }, menuAggregate); 
+      return reducedIngredient; 
+    }, aggregate); 
+    return reducedItems; 
+
+  }, zeroStockIngredients); 
+
+  console.log('stock order  ' + stockFromOrders);
+
+  // sum the stock from the purchases and the negative stock from the orders 
+  const stock = ingredientTypes.map(ingredient => { 
+    const qtyPurchase = purchasesStock.find(it => it._id == ingredient._id); 
+    const qtyOrder = stockFromOrders.get(ingredient._id); 
+    return { 
+      ingredient: ingredient._id, 
+      qty: qtyPurchase.total + qtyOrder, 
+    }; 
+  }); 
+
+  return stock;
 }
