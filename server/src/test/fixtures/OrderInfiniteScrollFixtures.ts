8b0d9e6b4357a8946dfb27f0e8391b46e3b517d7
Unknown <nigelnaiguel.comp@gmail.com>
2018-07-19 16:58:03 -0300

    Separated fixtures for orders infinite scroll

---
+++ server/src/test/fixtures/OrderInfiniteScrollFixtures.ts
@@ -0,0 +1,258 @@
+import { IOrder } from "../../db/models/Order";
+import { idByValue } from "../lib/TestUtils";
+import { MenuItem } from "../../db/models/menuItem";
+
+export default async (): Promise<IOrder[]> => {
+  return [
+    {
+      userId: "userid",
+      local: "Prédio 1",
+      localComplement: "apt 101",
+      comments: "Ótimo!!",
+      kitchenComments: null,
+      status: "PENDING",
+      paymentOption: "Dinheiro",
+      telephoneNumber: "1234",
+      totalAmount: 50.0,
+      items: [
+        {
+          qty: 2,
+          itemTotalPrice: 50.0,
+          foodMenuItem: {
+            id: await idByValue(MenuItem, "title", "Sanduba de frango"),
+            title: "Sanduba de frango",
+            description: "Muito bom",
+            price: 25.0
+          }
+        }
+      ],
+      statusHistory: [],
+      createdOn: new Date("2018-01-02")
+    },
+    {
+      userId: "userid",
+      local: "Prédio 2",
+      localComplement: "apt 903",
+      comments: "Ótimo!!",
+      kitchenComments: null,
+      status: "PENDING",
+      paymentOption: "Cartão",
+      telephoneNumber: "94345",
+      totalAmount: 48.0,
+      items: [
+        {
+          qty: 2,
+          itemTotalPrice: 48.0,
+          foodMenuItem: {
+            id: await idByValue(MenuItem, "title", "Sanduba de frango"),
+            title: "Sanduba de frango",
+            description: "Muito bom",
+            price: 24.0
+          }
+        }
+      ],
+      statusHistory: [],
+      createdOn: new Date("2018-01-03")
+    },
+    {
+      userId: "userid",
+      local: "Prédio 2",
+      localComplement: "apt 903",
+      comments: "",
+      kitchenComments: null,
+      status: "PENDING",
+      paymentOption: "Cartão",
+      telephoneNumber: "94345",
+      totalAmount: 15.0,
+      items: [
+        {
+          qty: 1,
+          itemTotalPrice: 15.0,
+          foodMenuItem: {
+            id: await idByValue(MenuItem, "title", "Sanduíche de Mignon"),
+            title: "Sanduíche de Mignon",
+            description: "Melhor não tem",
+            price: 15.0
+          }
+        }
+      ],
+      createdOn: new Date("2018-01-04"),
+      statusHistory: []
+    },
+    {
+      userId: "userid",
+      local: "Prédio 2",
+      localComplement: "apt 903",
+      comments: "",
+      kitchenComments: null,
+      status: "PENDING",
+      paymentOption: "Cartão",
+      telephoneNumber: "94345",
+      totalAmount: 8.0,
+      items: [
+        {
+          qty: 1,
+          itemTotalPrice: 8.0,
+          foodMenuItem: {
+            id: await idByValue(MenuItem, "title", "Açai"),
+            title: "Açai",
+            description: "Açai batido com banana e morango, vem cheião.",
+            price: 8.0
+          }
+        }
+      ],
+      statusHistory: [],
+      createdOn: new Date("2018-01-06")
+    },
+    {
+      userId: "userid",
+      local: "Prédio 2",
+      localComplement: "apt 111",
+      comments: "",
+      kitchenComments: null,
+      status: "PENDING",
+      paymentOption: "Cartão",
+      telephoneNumber: "42123",
+      totalAmount: 8.0,
+      items: [
+        {
+          qty: 1,
+          itemTotalPrice: 8.0,
+          foodMenuItem: {
+            id: await idByValue(MenuItem, "title", "Açai"),
+            title: "Açai",
+            description: "Açai batido com banana e morango, vem cheião.",
+            price: 8.0
+          }
+        }
+      ],
+      statusHistory: [],
+      createdOn: new Date("2018-01-07")
+    },
+    {
+      userId: "userid",
+      local: "Prédio 2",
+      localComplement: "apt 903",
+      comments: "Rapidao",
+      kitchenComments: null,
+      status: "PENDING",
+      paymentOption: "Cartão",
+      telephoneNumber: "94345",
+      totalAmount: 8.0,
+      items: [
+        {
+          qty: 1,
+          itemTotalPrice: 8.0,
+          foodMenuItem: {
+            id: await idByValue(MenuItem, "title", "Açai"),
+            title: "Açai",
+            description: "Açai batido com banana e morango, vem cheião.",
+            price: 8.0
+          }
+        }
+      ],
+      statusHistory: [],
+      createdOn: new Date("2018-01-08")
+    },
+    {
+      userId: "userid",
+      local: "Prédio 2",
+      localComplement: "apartamento 501",
+      comments: "Bem mais rapido",
+      kitchenComments: null,
+      status: "PENDING",
+      paymentOption: "Cartão",
+      telephoneNumber: "94345",
+      totalAmount: 8.0,
+      items: [
+        {
+          qty: 1,
+          itemTotalPrice: 15.0,
+          foodMenuItem: {
+            id: await idByValue(MenuItem, "title", "Sanduíche de Mignon"),
+            title: "Sanduíche de Mignon",
+            description: "Melhor não tem",
+            price: 15.0
+          }
+        }
+      ],
+      statusHistory: [],
+      createdOn: new Date("2018-02-01")
+    },
+    {
+      userId: "userid",
+      local: "Prédio 2",
+      localComplement: "apt 662",
+      comments: "Comentario",
+      kitchenComments: null,
+      status: "PENDING",
+      paymentOption: "Cartão",
+      telephoneNumber: "94345",
+      totalAmount: 15.0,
+      items: [
+        {
+          qty: 1,
+          itemTotalPrice: 15.0,
+          foodMenuItem: {
+            id: await idByValue(MenuItem, "title", "Sanduíche de Mignon"),
+            title: "Sanduíche de Mignon",
+            description: "Melhor não tem",
+            price: 15.0
+          }
+        }
+      ],
+      createdOn: new Date("2018-02-04"),
+      statusHistory: []
+    },
+    {
+      userId: "userid",
+      local: "Prédio 2",
+      localComplement: "apt 777",
+      comments: "segundo comentario",
+      kitchenComments: null,
+      status: "PENDING",
+      paymentOption: "Cartão",
+      telephoneNumber: "94345",
+      totalAmount: 15.0,
+      items: [
+        {
+          qty: 1,
+          itemTotalPrice: 15.0,
+          foodMenuItem: {
+            id: await idByValue(MenuItem, "title", "Sanduíche de Mignon"),
+            title: "Sanduíche de Mignon",
+            description: "Melhor não tem",
+            price: 15.0
+          }
+        }
+      ],
+      createdOn: new Date("2018-02-05"),
+      statusHistory: []
+    },
+    {
+      userId: "userid",
+      local: "Prédio 2",
+      localComplement: "apt 888",
+      comments: "fake",
+      kitchenComments: null,
+      status: "PENDING",
+      paymentOption: "Cartão",
+      telephoneNumber: "1111222333",
+      totalAmount: 15.0,
+      items: [
+        {
+          qty: 1,
+          itemTotalPrice: 15.0,
+          foodMenuItem: {
+            id: await idByValue(MenuItem, "title", "Sanduíche de Mignon"),
+            title: "Sanduíche de Mignon",
+            description: "Melhor não tem",
+            price: 15.0
+          }
+        }
+      ],
+      createdOn: new Date("2018-02-06"),
+      statusHistory: []
+    },
+  ];
+};
