9bda496275c42b3740bed0860b19d49a6902dc59
Unknown <nigelnaiguel.comp@gmail.com>
2018-07-20 17:49:38 -0300

    General config graph ql

---
+++ server/src/graphql/GeneralConfigGraphQL.ts
@@ -0,0 +1,36 @@
+import {
+    GraphQLObjectType,
+    GraphQLInputObjectType,
+    GraphQLNonNull,
+    GraphQLID,
+    GraphQLString,
+    GraphQLFloat,
+    GraphQLList,
+  } from 'graphql';
+  import * as logger from 'winston';
+  
+  import { GeneralConfig } from '../db/models/GeneralConfig';
+  import * as Auth from '../lib/Auth';
+  
+  const GeneralConfigCompleteType = new GraphQLObjectType({
+    name: 'GeneralConfigCompleteType',
+    fields: {
+      key: { type: GraphQLString },
+      value: { type: GraphQLString },
+    }
+  });
+  
+  export const Query = {
+    getByKey: {
+      type: GeneralConfigCompleteType,
+      args: {
+        key: { type: GraphQLString }
+      },
+      resolve: async function(root, { key }, source, fieldASTs) {
+        const generalConfig = await GeneralConfig.findOne({'key' : key});
+        return generalConfig;
+      }
+    }
+  };
+  
+  
