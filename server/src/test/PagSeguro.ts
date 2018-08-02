e4c8258fe89716ba65289290a5e19fa5f0e25301
Unknown <nigelnaiguel.comp@gmail.com>
2018-08-02 12:30:55 -0300

    Getting session id from xml string

--- server/src/test/PagSeguro.ts
+++ server/src/test/PagSeguro.ts
@@ -1,7 +1,5 @@
-// var PagSeguro = require('node-pagseguro');
-
-const PagSeguro = require('pagseguro-nodejs');
 const axios = require('axios');
+var parseString = require('xml2js').parseString;
 
 
 testPagseguro();
@@ -14,22 +12,8 @@ function param( params ) {
   return p.toString();
 }
 
-function doSomething(data) {
-  console.log(data);
-}
 
 function testPagseguro(){
-  console.log("start test");
-
-  // axios({
-  //   method:'get',
-  //   url:'http://bit.ly/2mTM3nY',
-  //   responseType:'stream'
-  // })
-  // .then(function(response) {
-  //   console.log(response.data.statusCode);
-  // });
-
   const reqData = {
     'email': 'guilherme.mst@gmail.com',
     'token': '6D17B04C51F749EEA3F3ECE500FE01C1'
@@ -48,114 +32,11 @@ function testPagseguro(){
     data: param(reqData)
   })
   .then(function(response) {
-    doSomething(response.data);
+    parseString(response.data, function (err, result) {
+      console.dir(result.session.id[0]);
+    });
   })
   .catch(function (error) {
     console.log(error.response.status)
   });
-
-  // var pagseguro = new PagSeguro({
-  //   email: 'guilherme.mst@gmail.com',
-  //   token: '6D17B04C51F749EEA3F3ECE500FE01C1',
-  //   mode: PagSeguro.MODE_SANDBOX,
-  //   debug: true
-  // });
-
-  // pagseguro.currency('BRL');
-  // pagseguro.reference('12345');
-
-  // /* Produtos */
-  // pagseguro.addItem({
-  //   id: '1',
-  //   description: 'Descrição do primeiro produto',
-  //   amount: '40.00',
-  //   quantity: '1'
-  // });
-
-  // pagseguro.addItem({
-  //   id: '2',
-  //   description: 'Descrição do segundo produto',
-  //   amount: '40.00',
-  //   quantity: '9'
-  // });
-
-
-  // pagseguro.sender({
-  //   name: 'Edvaldo Szymonek',
-  //   email: 'nigelnaiguel.comp@gmail.com',
-  //   phone: {
-  //     areaCode: '51',
-  //     number: '12345678'
-  //   }
-  // });
-
-  // pagseguro.shipping({
-  //   type: 1,
-  //   name: 'Edvaldo Szymonek',
-  //   email: 'nigelnaiguel.comp@gmail.com',
-  //   address: {
-  //     street: 'Endereço',
-  //     number: '10',
-  //     city: 'Nome da cidade',
-  //     state: 'PR',
-  //     country: 'BRA'
-  //   }
-  // });
-
-  // pagseguro.checkout(function(success, response) {
-  //   if (success) {
-  //     console.log('Success');
-  //     console.log(response);
-  //   } else {
-  //     console.log('Error');
-  //     console.error(response);
-  //   }
-  // });
-
-
-//   let payment = new PagSeguro({
-//     email: 'guilherme.mst@gmail.com',
-//     token: '6D17B04C51F749EEA3F3ECE500FE01C1',
-//     sandbox: true,
-//     sandbox_email: 'v26476421954760399948@sandbox.pagseguro.com.br'
-//  });
-
-//  console.log("payment " + payment);
-
-//   payment.setSender({
-//     name: 'Nigel da Silva Lima',
-//     email: 'comprador@sandbox.pagseguro.com.br',
-//     cpf_cnpj: "05575340384",
-//     area_code: '11',
-//     phone: '983329436',
-//     birth_date: '18/03/1994' //formato dd/mm/yyyy
-//   });
-
-//   payment.setShipping({
-//     street: 'Av Perimetral Sul',
-//     number: '501',
-//     district: 'District',
-//     city: 'Sao Luis',
-//     state: 'MA',
-//     postal_code: '65000000',
-//     same_for_billing: true //opcional, informar se o endereço de entrega for o mesmo do endereço de cobrança
-//   });
-
-//   payment.addItem({
-//     qtde: 1,
-//     value: 35,
-//     description: 'Item 1'
-//   });
-
-//   payment.sessionId(function(err, session_id) { });
-//   payment.sendTransaction({
-//       method: 'boleto', //'boleto' ou 'creditCard'
-//       value: 35,
-//       installments: 1, //opcional, padrão 1
-//       hash: 'rye2B5mp9T9yk12b' //senderHash gerado pela biblioteca do PagSeguro
-//     }, function(err, data) {
-//       console.log(data);
-//       console.log(err);
-//   });
-  
 };
