f06e87d63be1ce848e2d15afdbd0fc810c73b9ca
Unknown <nigelnaiguel.comp@gmail.com>
2018-08-29 02:22:13 -0300

    Mapping errors from pagseguro to readable front-end errors

---
+++ server/src/lib/PagSeguroErrors.ts
@@ -0,0 +1,57 @@
+
+export function mapPagseguroBadRequest(response): any {
+
+  let errors = {};
+
+  for(let i = 0; i < response.length; i++){
+    switch(response[i])
+    {
+      case "10000": 
+        errors['cardNumber'] = 'Número de cartão de crédito inválido.';
+        break;
+      case "10003": 
+        errors['cvv'] = 'Código de segurança inválido.';
+        break;
+      case "53087": 
+        errors['general'] = 'Dados de cartão inválidos.';
+        break;
+      case "53091": 
+        errors['general'] = 'Erro ao efetuar pagamento. Tente novamente.';
+        break;
+      case "53092": 
+        errors['cardNumber'] = 'Bandeira de cartão não aceitável.';
+        break;
+      case "53141": 
+        errors['senderName'] = 'Cliente bloqueado.';
+        break;
+      case "53142": 
+        errors['general'] =  'Erro ao efetuar pagamento. Tente novamente.';
+        break;
+      case "53020": 
+        errors['senderPhone'] =  'Campo telefone é obrigatório';
+        break;
+      case "53118": 
+        errors['senderCpf'] =  'Campo CPF é obrigatório';
+        break;
+      case "53013": 
+        errors['senderName'] =  'Campo Nome do Cliente é obrigatório';
+        break;
+      case "53010": 
+        errors['senderEmail'] =  'Campo Email do Cliente é obrigatório';
+        break;
+      case "53042": 
+        errors['creditCardHolderName'] =  'Campo Nome no Cartão é obrigatório';
+        break;
+      case "53045": 
+        errors['creditCardHolderCPF'] =  'Campo CPF do dono do cartão é obrigatório';
+        break;
+    }
+  }
+  console.log(errors);
+  return errors;
+}
+
+export function mapPagseguroBadRequestForCardToken(response): any {
+  const errors = mapPagseguroBadRequest(Object.keys(response));
+  return errors;
+}
