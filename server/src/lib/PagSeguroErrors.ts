bf2633719dae1c6d842969e9c013be0c925a002f
Unknown <nigelnaiguel.comp@gmail.com>
2018-09-07 12:03:36 -0300

    Finishing pagseguro errors map

--- server/src/lib/PagSeguroErrors.ts
+++ server/src/lib/PagSeguroErrors.ts
@@ -45,6 +45,34 @@ export function mapPagseguroBadRequest(response): any {
       case "53045": 
         errors['creditCardHolderCPF'] =  'Campo CPF do dono do cartão é obrigatório';
         break;
+      case "5003": 
+        errors['general'] =  'Falha de comunicação com a instituição financeira';
+        break;
+      case "10001": 
+        errors['cardNumber'] =  'Número do cartão com tamanho inválido';
+        break;
+      case "10002": 
+        errors['expirationDate'] =  'Data no formato inválido';
+        break;
+      case "10004": 
+        errors['cvv'] =  'Campo Código de Segurança é obrigatório';
+        break;
+      case "10006": 
+        errors['cvv'] =  'Campo Código de Segurança com tamanho inválido';
+        break;
+      case "53011": 
+        errors['senderEmail'] =  'Campo Email com tamanho inválido';
+        break;
+      case "53012": 
+        errors['senderEmail'] =  'Email com formato inválido';
+        break;
+      case "53017": 
+        errors['senderCpf'] =  'CPF inválido';
+        break;
+      case "53141": 
+        errors['general'] =  'Usuário bloqueado pela instituição financeira';
+        break;
+      
     }
   }
   console.log(errors);
